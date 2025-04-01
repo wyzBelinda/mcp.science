23f67ed579556eed178143d26f26b4fcf81a3316
rong-xyz <rong@pathintegral.xyz>
2025-04-01 03:14:56 +0000

    add argparse

--- servers/python_code_execution/src/mcp_python_code_execution/__init__ .py
+++ servers/python_code_execution/src/mcp_python_code_execution/__init__ .py
@@ -1,10 +1,20 @@
 import asyncio
+import argparse
 
 from .server import serve
 
 
 def main():
-    asyncio.run(serve())
+    parser = argparse.ArgumentParser("a general python code execution tool")
+
+    parser.add_argument(
+        "--code",
+        type=str,
+        help="code to execute"
+    )
+    args = parser.parse_args()
+
+    asyncio.run(serve(args.code))
 
 if __name__ == "__main__":
     main()
